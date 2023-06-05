d5f1ec2b611d45ee5f49d1173cda7d11aab6c8d0
Ong Jun Feng <ongjunfeng17@gmail.com>
2023-06-05 15:03:58 +0800

    first commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ong Jun Feng on 25/5/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
